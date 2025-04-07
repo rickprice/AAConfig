c5db6dfbad7de99461c874d2a6487ee29776aa08
Frederick Price <rprice@pricemail.ca>
2025-04-06 21:09:30 -0400

    Latest updates that seem to work
    However, it may be always rebooting, not sure

--- ArchLinux/ansible-arch-update/update-all.sh
+++ ArchLinux/ansible-arch-update/update-all.sh
@@ -1,6 +1,6 @@
 
 #!/usr/bin/env bash
 
-ansible-playbook -i inventory.ini update-arch.yml --ask-become-pass --ask-pass
+ansible-playbook -i inventory.ini update-arch.yml --ask-become-pass
 # ansible-playbook -i inventory.ini update-arch.yml --ask-become-pass -vvvv
 
