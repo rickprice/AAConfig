5366e828316a2445c08686a649323c79b1c080ee
Frederick Price <rprice@pricemail.ca>
2025-04-14 10:04:18 -0400

    Make sure Git has a password

--- ArchLinux/ansible-arch-update/reboot-all.sh
+++ ArchLinux/ansible-arch-update/reboot-all.sh
@@ -1,5 +1,8 @@
 
 #!/usr/bin/env bash
 
+# Make sure that Git has a password
+git pull
+
 ansible-playbook -i inventory.ini reboot-all.yml --ask-become-pass
 
--- ArchLinux/ansible-arch-update/update-all.sh
+++ ArchLinux/ansible-arch-update/update-all.sh
@@ -1,6 +1,9 @@
 
 #!/usr/bin/env bash
 
+# Make sure that Git has a password
+git pull
+
 ansible-playbook -i inventory.ini update-arch.yml --ask-become-pass
 # ansible-playbook -i inventory.ini update-arch.yml --ask-become-pass -vvvv
 
